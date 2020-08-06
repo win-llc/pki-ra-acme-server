e4e655f06ffe26a16be6c9bc43f8381a075b408d
Davd Winters <jrmints22@gmail.com>
2020-08-06 19:58:36 -0400

    update for compat

--- src/main/java/com/winllc/acme/server/challenge/DnsChallenge.java
+++ src/main/java/com/winllc/acme/server/challenge/DnsChallenge.java
@@ -10,6 +10,7 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.xbill.DNS.*;
+import org.xbill.DNS.Record;
 
 
 //Section 8.4
